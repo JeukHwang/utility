0051d6d3939c59903c77a422ea061fabc577edd3
Jeuk Hwang <jeukhwang.dev@gmail.com>
2022-04-08 01:20:32 +0900

    feat: random util

---
+++ code/random.ts
@@ -0,0 +1,16 @@
+function getRandomInt(min: number, max:number):number {
+    const minInt = Math.ceil(min);
+    const maxInt = Math.floor(max);
+    return Math.floor(Math.random() * (maxInt - minInt)) + minInt;
+}
+
+function shuffleArray<T>(array:Array<T>):void {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j]!, array[i]!];
+    }
+}
+
+export { getRandomInt };
+export { shuffleArray };
+
